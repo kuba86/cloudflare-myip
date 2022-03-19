653eb21ee26a50944a58f00b3318c50060940d6b
kuba86 <kuba86@gmail.com>
2022-03-19 11:27:12 +0100

    js copy function const name refactor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -68,8 +68,8 @@ object Main {
        |<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
        |<script>
        |  function copyTxt(id) {
-       |    const email = document.getElementById(id).value;
-       |    navigator.clipboard.writeText(email);
+       |    const text = document.getElementById(id).value;
+       |    navigator.clipboard.writeText(text);
        |  }
        |</script>
        |</body>
